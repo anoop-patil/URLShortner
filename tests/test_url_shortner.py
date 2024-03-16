24ae31fd6aa397e08ed1d316cff088f913c60bd2
anoop-patil <anooppatils@gmail.com>
2024-03-16 14:09:21 +0000

    Add pre-commit

--- tests/test_url_shortner.py
+++ tests/test_url_shortner.py
@@ -5,44 +5,46 @@ import redis
 
 class TestURLShortening(TestCase):
     def create_app(self):
-        app.config['TESTING'] = True
+        app.config["TESTING"] = True
         # Override any other configurations for testing
-        app.config['REDIS_DB'] = 10  # Use a different database index for tests
+        app.config["REDIS_DB"] = 10  # Use a different database index for tests
         return app
 
     def setUp(self):
-        self.redis_client = redis.Redis(host='localhost', port=6379, db=app.config['REDIS_DB'])
+        self.redis_client = redis.Redis(
+            host="localhost", port=6379, db=app.config["REDIS_DB"]
+        )
         self.redis_client.flushdb()
 
     def tearDown(self):
         pass
 
     def test_shorten_url(self):
-        response = self.client.post('/', json={'url': 'https://example.com'})
+        response = self.client.post("/", json={"url": "https://example.com"})
         self.assertEqual(response.status_code, 201)
-        self.assertIn('short_url', response.json)
-        self.assertIn('http://localhost/', response.json['short_url'])
+        self.assertIn("short_url", response.json)
+        self.assertIn("http://localhost/", response.json["short_url"])
 
     def test_redirect_to_original_url(self):
-        long_url = 'https://www.example.com'
-        response = self.client.post('/', json={'url': long_url})
-        short_key = response.json['key']
+        long_url = "https://www.example.com"
+        response = self.client.post("/", json={"url": long_url})
+        short_key = response.json["key"]
 
-        response = self.client.get(f'/{short_key}')
+        response = self.client.get(f"/{short_key}")
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.location, long_url)
 
     def test_delete_shortened_url(self):
 
-        long_url = 'https://www.example.com'
-        response = self.client.post('/', json={'url': long_url})
-        short_key = response.json['key']
+        long_url = "https://www.example.com"
+        response = self.client.post("/", json={"url": long_url})
+        short_key = response.json["key"]
 
-        response = self.client.delete(f'/{short_key}')
+        response = self.client.delete(f"/{short_key}")
 
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.get(f'/{short_key}')
+        response = self.client.get(f"/{short_key}")
 
         self.assertEqual(response.status_code, 404)
--- url_shortner.py
+++ url_shortner.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify, redirect
 import redis
 import hashlib
 
+
 class URLRepository:
     def __init__(self):
-        self.redis_client = redis.Redis(host='localhost', port=6379, db=0)
+        self.redis_client = redis.Redis(host="localhost", port=6379, db=0)
 
     def get_long_url(self, short_key):
         return self.redis_client.get(short_key)
@@ -15,6 +16,7 @@ class URLRepository:
     def delete_url(self, short_key):
         return self.redis_client.delete(short_key)
 
+
 class URLShorteningService:
     def __init__(self, repo: URLRepository):
         self.repo = repo
@@ -22,7 +24,10 @@ class URLShorteningService:
     def shorten_url(self, long_url):
         hash_object = hashlib.sha256(long_url.encode())
         short_key = hash_object.hexdigest()[:6]
-        while self.repo.get_long_url(short_key) and self.repo.get_long_url(short_key).decode('utf-8') != long_url:
+        while (
+            self.repo.get_long_url(short_key)
+            and self.repo.get_long_url(short_key).decode("utf-8") != long_url
+        ):
             hash_object = hashlib.sha256((long_url + short_key).encode())
             short_key = hash_object.hexdigest()[:6]
         self.repo.store_url(short_key, long_url)
@@ -34,38 +39,43 @@ class URLShorteningService:
     def delete_short_url(self, short_key):
         return self.repo.delete_url(short_key)
 
+
 app = Flask(__name__)
 repo = URLRepository()
 service = URLShorteningService(repo)
 
-@app.route('/', methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def shorten_url_endpoint():
     data = request.get_json()
-    if not data or 'url' not in data:
+    if not data or "url" not in data:
         return jsonify({"Error": "Missing Field: URL"}), 400
     short_key, long_url = service.shorten_url(data["url"])
     response_data = {
         "key": short_key,
         "long_url": long_url,
-        "short_url": f"http://localhost/{short_key}"
+        "short_url": f"http://localhost/{short_key}",
     }
     return jsonify(response_data), 201
 
-@app.route('/<short_key>', methods=['GET'])
+
+@app.route("/<short_key>", methods=["GET"])
 def redirect_short_url(short_key):
     long_url = service.get_long_url(short_key)
     if long_url:
-        return redirect(long_url.decode('utf-8'), code=302)
+        return redirect(long_url.decode("utf-8"), code=302)
     else:
         return "URL not found", 404
 
-@app.route('/<short_key>', methods=['DELETE'])
+
+@app.route("/<short_key>", methods=["DELETE"])
 def delete_short_url(short_key):
     result = service.delete_short_url(short_key)
     if result == 1:
-        return '', 200
+        return "", 200
     else:
         return "URL not found", 404
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
